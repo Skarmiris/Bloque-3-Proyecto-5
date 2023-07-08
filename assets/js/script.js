a43c0084b20aad90576ea9a2e27f8b74e93d4e6f
carlosd.mutis@gmail.com <carlosd.mutis@gmail.com>
2023-07-07 23:59:39 -0400

    modificacion css

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+
+
+let Tareas = [
+    { id: 1, description: "Lavar la ropa", completed: false },
+    { id: 2, description: "Pasear al perro", completed: false },
+    { id: 3, description: "Bañar al tigre", completed: false }
+];
+
+const listTareas = document.getElementById("listaTareas");
+const totalTareas = document.getElementById("totalTareas");
+const totalTareasRealizadas = document.getElementById("totalTareasRealizadas");
+const presBtn = document.getElementById("presBtn");
+
+function ListaRealizada() {
+    let listaHtml = "";
+
+    Tareas.forEach((tarea) => {
+        const tareaHtml = `
+          <li id="${tarea.id}">
+            <label>${tarea.description}</label> 
+            <input type="checkbox" onclick="marcarTarea(${tarea.id})" ${tarea.completed ? 'checked' : ''}>
+            <button onclick="borrarTarea(${tarea.id})">Borrar</button>
+          </li>`;
+        listaHtml += tareaHtml;
+    });
+    listTareas.innerHTML = listaHtml;
+    totalTareas.textContent = Tareas.length;
+    totalTareasRealizadas.textContent = Tareas.filter(tarea => tarea.completed).length;
+};
+
+function agregarTarea() {
+    const inputTarea = document.getElementById("inputTarea"); 
+    const descripcion = inputTarea.value.trim();
+
+    if (descripcion !== "") {
+        const id = Tareas.length + 1;
+        const nuevaTarea = {
+            id: id, description: descripcion,
+            completed: false
+        };
+
+        Tareas.push(nuevaTarea);
+        inputTarea.value = "";
+        ListaRealizada();
+    }
+};
+
+ListaRealizada();
+
+function borrarTarea(id) {
+    const index = Tareas.findIndex(tarea => tarea.id === id);
+
+    if (index !== -1) {
+        Tareas.splice(index, 1);
+        ListaRealizada();
+    }
+};
+
+function marcarTarea(id) {
+    const tarea = Tareas.find(tarea => tarea.id === id);
+
+    if (tarea) {
+        tarea.completed = !tarea.completed;
+        ListaRealizada();
+    }
+};
+
+presBtn.addEventListener("click", agregarTarea);
